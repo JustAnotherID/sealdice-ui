c63238183aab36bf7766738fcb80db899fad8e0f
JustAnotherID <just-another-id@outlook.com>
2024-10-22 01:52:33 +0800

    feat: 开始引入 naive ui，增加夜间模式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { darkTheme, lightTheme } from 'naive-ui';
 import { useStore } from '~/store';
-import Main from '~/Main.vue';
+import MainBox from '~/MainBox.vue';
 
+const isDark = useDark();
 const store = useStore();
 
 const testMode = ref<boolean>(false);
@@ -13,12 +15,27 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="mx-auto w-full h-full">
-    <el-watermark
-      :content="testMode ? '仅用于展示，修改无效' : ''"
-      :font="{ fontSize: 24 }"
-      :gap="[100, 32]">
-      <Main />
-    </el-watermark>
-  </div>
+  <n-config-provider :theme="isDark ? darkTheme : lightTheme" class="mx-auto w-full h-full">
+    <n-dialog-provider>
+      <n-message-provider>
+        <n-modal-provider>
+          <n-notification-provider>
+            <n-watermark
+              :content="testMode ? '仅用于展示 修改无效' : ''"
+              :cross="true"
+              :font-size="22"
+              :height="300"
+              :line-height="22"
+              :rotate="-15"
+              :width="240"
+              :x-offset="12"
+              :y-offset="60"
+              full-screen>
+              <MainBox />
+            </n-watermark>
+          </n-notification-provider>
+        </n-modal-provider>
+      </n-message-provider>
+    </n-dialog-provider>
+  </n-config-provider>
 </template>
