fdb19de612aae7e9f7c9342d773bf7cb55a5c819
JustAnotherID <just-another-id@outlook.com>
2024-11-08 23:39:23 +0800

    build: remove tailwindcss-theme-variants

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,9 @@
 import type { Config } from 'tailwindcss';
-import { themeVariants } from 'tailwindcss-theme-variants';
 
 export default {
   content: ['./index.html', './src/**/*.{html,js,ts,jsx,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    themeVariants({
-      themes: {
-        light: {
-          selector: '.light',
-        },
-        dark: {
-          selector: '.dark',
-        },
-      },
-    }),
-  ],
+  plugins: [],
 } satisfies Config;
