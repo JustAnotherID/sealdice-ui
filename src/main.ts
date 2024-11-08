2ac5b9186a2ffb27a8b9616853d2085f70bfa4ad
JustAnotherID <just-another-id@outlook.com>
2024-11-08 13:54:05 +0800

    feat: 开始引入 naive ui，增加夜间模式

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,15 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-
-// import "~/styles/element/index.scss";
-
-// import ElementPlus from "element-plus";
-// import all element css, uncommented next line
-// import "element-plus/dist/index.css";
-
-// or use cdn, uncomment cdn link in `index.html`
-
 import '~/styles/index.css';
-
-// If you want to use ElMessage, import it.
-// import "element-plus/theme-chalk/src/message.scss"
-// import "element-plus/es/components/message-box/style/css"
-// import "element-plus/es/components/dialog/style/css"
-// import 'element-plus/theme-chalk/display.css'
 import './str.polyfill';
-
+import { createApp } from 'vue';
 import { useStore } from '~/store';
-const loading = useStorage('router-view-loading', true);
+import App from './App.vue';
 import router from '~/router';
+import VueDiff from 'vue-diff';
+import 'vue-diff/dist/index.css';
+import yaml from 'highlight.js/lib/languages/yaml';
+import toml from 'highlight.js/lib/languages/ini';
+
+const loading = useStorage('router-view-loading', true);
 
 router.beforeEach((to, from, next) => {
   loading.value = true;
@@ -32,11 +21,6 @@ router.afterEach((to, from) => {
     loading.value = false;
   }, 300);
 });
-
-import VueDiff from 'vue-diff';
-import 'vue-diff/dist/index.css';
-import yaml from 'highlight.js/lib/languages/yaml';
-import toml from 'highlight.js/lib/languages/ini';
 
 const app = createApp(App);
 app.use(createPinia());
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,21 @@
 import type { Config } from 'tailwindcss';
+import { themeVariants } from 'tailwindcss-theme-variants';
 
 export default {
-  content: ['./index.html', './src/**/*.{html,js,ts,vue}'],
+  content: ['./index.html', './src/**/*.{html,js,ts,jsx,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    themeVariants({
+      themes: {
+        light: {
+          selector: '.light',
+        },
+        dark: {
+          selector: '.dark',
+        },
+      },
+    }),
+  ],
 } satisfies Config;
